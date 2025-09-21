79675e5ebfca87d055d1e2caf0d009f89f5091a0
ljpprojects <owner@ljpprojects.org>
2025-09-22 07:14:37 +1000

    BIGNUMBER
    Signed-off-by: ljpprojects <owner@ljpprojects.org>

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
 set -euo pipefail
-mkdir -p site/dist
+mkdir -p site/dist/lib
 
 function buildts {
     bun ./node_modules/typescript/bin/tsc;
-    ./node_modules/esbuild/bin/esbuild --format=iife --bundle --minify --target=es5 --outfile=site/dist/bundle.js site/dist/game.js
+    cp ./site/ts/lib/bignumber.min.js ./site/dist/lib/bignumber.js;
+    ./node_modules/esbuild/bin/esbuild --format=iife --bundle --minify --target=es6 --outfile=site/dist/bundle.js site/dist/game.js
 }
 
 function buildcsshtml {
--- site/ts/jokes.ts
+++ site/ts/jokes.ts
@@ -1,5 +1,10 @@
 import { hds, hdnw } from "./game"
 import { taxPopupElement } from "./elements";
+import BigNumber from "./lib/bignumber";
+
+BigNumber.config({
+  DECIMAL_PLACES: 48,
+})
 
 export const doJoke = () => {
   const time = new Date(Date.now())
@@ -15,6 +20,11 @@ export const doJoke = () => {
   if ((hour + second - minute) % 2 === 0 || (hourUTC + secondUTC - minuteUTC) % 2 === 0) {
     // Tax the player
     taxationJoke()
+  }
+
+  if ([...(hour + minute - second).toString(16)].some([..."0679A"].includes)) {
+    // Make all buttons 'run away' from the mouse for 1 minute
+    fleeJoke()
   }
 
   setTimeout(doJoke, 42069 * Math.random())
@@ -79,4 +89,61 @@ const taxationJoke = () => {
   taxPopupElement.classList.remove("hide")
 
   setTimeout(() => taxPopupElement.classList.add("hide"), 3000)
+}
+
+const fleeJoke = () => {
+  // Get every button
+  const buttons = document.getElementsByTagName("button")
+
+  // Get the length of the diagonal(s) of the viewport
+  const viewportDiagonal = BigNumber(window.innerHeight).pow(2).plus(BigNumber(window.innerWidth).pow(2)).sqrt()
+
+  window.onmousemove = e => {
+    const [mouseX, mouseY] = [BigNumber(e.x), BigNumber(e.y)]
+
+    for (const button of buttons) {
+      const bounds = button.getBoundingClientRect()
+      const [centreX, centreY] = [BigNumber(bounds.left).plus(BigNumber(bounds.width).div(2)), BigNumber(bounds.top).plus(BigNumber(bounds.height).div(2))];
+
+      if ("original-cen-x"! in button.dataset) {
+        button.dataset["original-cen-x"] = centreX.toString()
+      }
+
+      if ("original-cen-y"! in button.dataset) {
+        button.dataset["original-cen-y"] = centreY.toString()
+      }
+
+      const [originalCentreX, originalCentreY] = [BigNumber(button.dataset["original-cen-x"]!), BigNumber(button.dataset["original-cen-y"]!)]
+
+      // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+      // MATHEMATICS
+      // I hadn't had any uses for the Pyhtagorean theorem until now
+
+      const a1 = mouseY.minus(centreY).abs()
+      const b1 = mouseX.minus(centreX).abs()
+      const c1 = a1.pow(2).plus(b1.pow(2)).sqrt()
+
+      const relativeDist = c1.div(viewportDiagonal);
+
+      // EVEN
+      // MORE
+      // MATHEMATICS
+      // I did NOT sign up for this much MATHEMATICS when I thought of this joke
+      // This time we need to get the distnace the button has strayed from its
+      // original position
+      const a2 = centreY.minus(originalCentreY).abs()
+      const b2 = centreX.minus(originalCentreX).abs()
+      const distanceStrayed = a2.pow(2).plus(b2.pow(2)).sqrt()
+
+      const th = BigNumber(200)
+      const tw = BigNumber(100)
+      const l = BigNumber(1.35)
+      const k = th.div(tw.pow(l))
+
+      const relativeDistanceToMoveAway = th.div(2).minus(relativeDist.times(100).pow(l)).times(k)
+      const distanceToMoveAway = relativeDistanceToMoveAway.times(relativeDistanceToMoveAway.lt(0) ? distanceStrayed : viewportDiagonal)
+
+      // Now to actually MOVE the button instead of doing MATHEMATICS
+    }
+  }
 }
