3d322499090905d460fa5d50fc4fc519f20bed62
ljpprojects <owner@ljpprojects.org>
2025-09-22 19:45:16 +1000

    Fix build?
    Signed-off-by: ljpprojects <owner@ljpprojects.org>

--- build.ps1
+++ build.ps1
@@ -1,11 +1,11 @@
 mkdir -p ./site/dist/
 mkdir -p ./site/dist/lib/
 
-bun ./node_modules/typescript/bin/tsc;
+~/.bun/bin/bun ./node_modules/typescript/bin/tsc;
 cp ./site/ts/lib/bignumber.min.js ./site/dist/lib/bignumber.js;
 ./node_modules/esbuild/bin/esbuild --format=iife --bundle --minify --target=es6 --outfile=site/dist/bundle.js site/dist/game.js
 
-bun ./node_modules/clean-css-cli/bin/cleancss -O3 -b site/index.css -o site/dist;
-bun ./node_modules/clean-css-cli/bin/cleancss -O3 -b site/alto.css -o site/dist;
-bun ./node_modules/clean-css-cli/bin/cleancss -O3 -b site/empty.css -o site/dist;
-bun ./node_modules/html-minifier/cli.js site/index.max.html --collapse-whitespace -o site/index.html;
+~/.bun/bin/bun ./node_modules/clean-css-cli/bin/cleancss -O3 -b site/index.css -o site/dist;
+~/.bun/bin/bun ./node_modules/clean-css-cli/bin/cleancss -O3 -b site/alto.css -o site/dist;
+~/.bun/bin/bun ./node_modules/clean-css-cli/bin/cleancss -O3 -b site/empty.css -o site/dist;
+~/.bun/bin/bun ./node_modules/html-minifier/cli.js site/index.max.html --collapse-whitespace -o site/index.html;
