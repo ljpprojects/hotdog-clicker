c302b4da6583f57eada4889582d056139a21934a
ljpprojects <owner@ljpprojects.org>
2025-11-29 19:34:38 +1100

    oh shyt the thign does not work in prod !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- worker/index.ts
+++ worker/index.ts
@@ -77,11 +77,7 @@ app.use(trimTrailingSlash());
 if (env.ENVIRONMENT.startsWith("prod:")) {
   app.use(cors({
     origin: env.ENVIRONMENT === "prod:release" ? 'https://hdc.ljpprojects.org' : 'https://dev.hdc.ljpprojects.org',
-    allowHeaders: ['Upgrade-Insecure-Requests'],
-    allowMethods: ['POST', 'GET'],
     exposeHeaders: ['Content-Length'],
-    maxAge: 600,
-    credentials: true,
   }))
 }
 
