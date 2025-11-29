e63c5cb35494c1d3ef257dd9a77a352c04eb6c9a
ljpprojects <owner@ljpprojects.org>
2025-11-29 20:23:05 +1100

    Add clairifier for EAUTH errors

--- worker/index.ts
+++ worker/index.ts
@@ -214,7 +214,7 @@ app.post("/api", async (c) => {
         success: false,
         error: {
           abbrev: "EAUTH",
-          message: "Must be authenticated to run an action.",
+          message: "Must be authenticated to run an action; your session is invalid.",
         },
       }),
     );
