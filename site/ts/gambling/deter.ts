031eb62fe7daee1ce03a095341f1854aaba9d408
ljpprojects <owner@ljpprojects.org>
2026-03-27 12:15:14 +1100

    its staying

--- site/ts/gambling/deter.ts
+++ site/ts/gambling/deter.ts
@@ -42,7 +42,7 @@ const targets = [
  *
  * @returns The relative distance to retreat.
  */
-const runSpeedCurve = (d: number) => 0.3 - d;
+const runSpeedCurve = (d: number) => 0.2 - d;
 
 type Point = [number, number];
 
